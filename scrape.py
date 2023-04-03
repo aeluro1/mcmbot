6131dfa4f28ef0435ee889e5861f8bb90186659e
aeluro1 <103622874+aeluro1@users.noreply.github.com>
2023-04-03 15:51:18 -0400

    Check in

--- scrape.py
+++ scrape.py
@@ -1,1 +1,14 @@
 import requests
+
+def main():
+    auth = {"Username": "john924xps@gmail.com", "Password": "mcmapitest"}
+    response = -1
+    try:
+        response = requests.post("https://api.mcmaster.com/v1/login", json = auth)
+    except requests.exceptions.RequestException as err:
+        print(err)
+    print(response)
+    
+
+if __name__ == "__main__":
+    main()
